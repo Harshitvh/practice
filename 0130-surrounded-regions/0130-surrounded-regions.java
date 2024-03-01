219dc218d4934ff81e3f8ec6948b2a5a88a205a5
Harshit Hawaldar <hvh208@gmail.com>
2024-03-01 22:03:27 +0530

    Time: 4 ms (22.98%), Space: 45.6 MB (34.85%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,47 +1,54 @@
 class Solution {
     public void solve(char[][] board) {
-        // Three things: 1. Capture unsurrounded area, dfs
-        // 2. Iterate and flip o to x
-        // 3. Flip t to o
-
+        Set<Pair> visited = new HashSet<>();
+        // Here we go to all the boarder and see if the zero is on boarder and an other
+        // zero is
+        // adjacent to it
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                if (i == 0 || i == board.length - 1 || j == 0 || j == board[i].length - 1) {
-                    if (board[i][j] == 'O') {
-                        dfs(i, j, board);
-                    }
+                if (i == 0 || j == 0 || i == board.length - 1 || j == board[i].length - 1) {
+                    if(board[i][j]=='O')
+                    dfs(board, i, j, visited);
                 }
             }
         }
+
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == 'O')
+                if (board[i][j] == 'O') {
                     board[i][j] = 'X';
+                }
             }
         }
+
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == 'T')
+                if (board[i][j] == 'T') {
                     board[i][j] = 'O';
+                }
             }
         }
     }
 
-    public void dfs(int i, int j, char[][] board) {
-        if ((i < 0 || i >= board.length || j < 0 || j >= board[i].length)) {
+    private void dfs(char[][] board, int i, int j, Set<Pair> visited) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[i].length) {
             return;
         }
-        if(board[i][j]!='O')
+        Pair pair = new Pair(i, j);
+        if(visited.contains(pair))
         {
             return;
         }
-
+     
+        if (board[i][j] != 'O') {
+            return;
+        }
         board[i][j] = 'T';
-        dfs(i + 1, j, board);
-        dfs(i - 1, j, board);
-        dfs(i, j + 1, board);
-        dfs(i, j - 1, board);
-
+        visited.add(pair);
+        dfs(board, i + 1, j, visited);
+        dfs(board, i - 1, j, visited);
+        dfs(board, i, j + 1, visited);
+        dfs(board, i, j - 1, visited);
     }
 
 }
