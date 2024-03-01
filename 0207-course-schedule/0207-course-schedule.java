c70b210efceed441ae99bff13624f31c7cff06d9
Harshit Hawaldar <hvh208@gmail.com>
2024-03-02 00:46:29 +0530

    Time: 9 ms (21.46%), Space: 45.1 MB (47.66%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,47 +1,53 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        if (prerequisites.length == 0) {
-            return true;
-        }
         Map<Integer, List<Integer>> map = buildGraph(prerequisites);
-        int sum = 0;
-        for (Integer key : map.keySet()) {
-            if (!bfs(key, map))
+        Set<Integer> visited = new HashSet<>();
+        Set<Integer> visiting = new HashSet<>();
+        for (int i = 0; i < numCourses; i++) {
+            boolean res = dfs(i, visited, visiting, map);
+            if (!res)
                 return false;
         }
         return true;
-    }
+   }
 
-    public boolean bfs(Integer key, Map<Integer, List<Integer>> graph) {
-        Set<Integer> visited = new HashSet<>();
-        LinkedList<Integer> q = new LinkedList<>();
-        visited.add(key);
-        q.add(key);
-        while (!q.isEmpty()) {
-            Integer pop = q.removeFirst();
-            List<Integer> nbors = graph.get(pop);
-            for (Integer n : nbors) {
-                if (n == key) {
-                    return false;
-                }
-                if (!visited.contains(n)) {
-                    q.add(n);
-                    visited.add(n);
-                }
-            }
+    private boolean dfs(int i, Set<Integer> visited, Set<Integer> visiting,
+            Map<Integer, List<Integer>> map) {
+        if (visiting.contains(i)) {
+            return false;
         }
+        if (visited.contains(i)) {
+            return true;
+        }
+
+        List<Integer> nbors = map.get(i) != null ? map.get(i) : new ArrayList<>();
+        visiting.add(i);
+        for (int z = 0; z < nbors.size(); z++) {
+          boolean res = dfs(nbors.get(z), visited,visiting,map);
+          if(!res)
+          {
+              return false;
+          }
+        }
+        visiting.remove(i);
+        visited.add(i);
         return true;
     }
 
-    private Map<Integer, List<Integer>> buildGraph(int[][] reqs) {
+    private Map<Integer, List<Integer>> buildGraph(int[][] reqs)
+    {
         Map<Integer, List<Integer>> map = new HashMap<>();
-        for (int i = 0; i < reqs.length; i++) {
-            int f = reqs[i][0];
-            int s = reqs[i][1];
-            map.putIfAbsent(f, new ArrayList<>());
-            map.putIfAbsent(s, new ArrayList<>());
-            map.get(f).add(s);
+        for(int i=0;i<reqs.length;i++)
+        {
+          
+                int f = reqs[i][0];
+                int s = reqs[i][1];
+                map.putIfAbsent(f, new LinkedList<>());
+                map.putIfAbsent(s, new LinkedList<>());
+                map.get(f).add(s);
+            
         }
         return map;
     }
+
 }
