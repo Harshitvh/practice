5e802d75669c1f7af96c9b9bcf83bfccabae3552
Harshit Hawaldar <hvh208@gmail.com>
2024-03-23 22:28:37 +0530

    Time: 16 ms (6.85%), Space: 44.4 MB (80.66%) - LeetHub

---
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        String s1 = word1;
+        String s2 = word2;
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+
+        System.out.println(s1);
+        System.out.println(s2);
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return (s1.length() + s2.length()) - (2 * dp[s1.length()][s2.length()]);
+    }
+}
